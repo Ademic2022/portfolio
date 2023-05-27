37c35f594e2cb8d899e4afdca73bf9739bbfa8b1
ademic2022 <adeeyomichael77@gmailcom>
2023-05-27 03:08:48 -0700

    updated js

--- portfolio/portfolio.js
+++ portfolio/portfolio.js
@@ -7,7 +7,7 @@ const containerBox = document.querySelector(".container-box")
 const elements = document.querySelector(".section-content")
 const header = document.querySelector("#header")
 const slider = document.querySelectorAll(".slider")
-const a = document.querySelectorAll(".a")
+const home_btn = document.querySelector("#home-btn")
 
 
 registerLink.addEventListener("click", ()=> {
@@ -29,7 +29,8 @@ closeIcon.addEventListener("click", ()=> {
 
 window.onscroll = function () { 
     if (document.body.scrollTop >= 100 || document.documentElement.scrollTop >= 100) {
-        header.classList.add("height")     
+        header.classList.add("height")
+        home_btn.classList.add('active')
     } 
     else {
         header.classList.remove("height")
@@ -48,3 +49,18 @@ slider.forEach((e) => {
         e.children[1].style.color = '#162938'
     })
 })
+window.addEventListener('scroll', function() {
+    var navButtons = document.querySelectorAll('.nav-btn');
+    var sections = document.querySelectorAll('section');
+  
+    sections.forEach(function(section, index) {
+      var rect = section.getBoundingClientRect();
+      console.log(rect);
+      if (rect.top <= 0 && rect.top <= window.innerHeight || window.scrollY === 0) {
+        navButtons.forEach(function(btn) {
+          btn.classList.remove('active');
+        });
+        navButtons[index].classList.add('active');
+      }
+    });
+});
